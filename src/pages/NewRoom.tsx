4d99b3221c0bed96a9d3324337844ef3de7bade1
Bruno Cezar Guimarães <bruno_guimaraes007@hotmail.com>
2021-06-22 23:41:55 -0300

    commit

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,16 +1,43 @@
-import { Link } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
 
 import { Button } from '../components/Button';
-//import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 export function NewRoom() {
-    //const { user } = useAuth();
+    const { user } = useAuth();
+    const history = useHistory();
+
+    //serve a ter acesso ao valor do input, mais especificamente ao nome da sala
+    const [newRoom, setNewRoom] = useState('');
+
+    //Prevene o comportamento padrao do formulario de enviar a uma outra pagina
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        //esse if impede de criar salas sem nomes, o trim tira os espaços na string
+        if (newRoom.trim() === '') {
+            return;
+        }
+
+        //referencia a dados dentro do banco de dados, nesse caso rooms, mais especificamente esta criando uma categoria rooms
+        const roomRef = database.ref('rooms');
+
+        //aqui ira jogar os dados para criar uma nova room no banco de dados
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id,
+        })
+
+        history.push(`/rooms/${firebaseRoom.key}`)
+    }
 
     return (
         <div id="page-auth">
@@ -25,10 +52,12 @@ export function NewRoom() {
                     <h2>Criar uma nova sala</h2>
                 </div>
 
-                <form>
+                <form onSubmit={handleCreateRoom}>
                     <input
                         type="text"
                         placeholder="Nome da sala"
+                        onChange={event => setNewRoom(event.target.value)}
+                        value={newRoom}
                     />
                     <Button type="submit">
                         Criar sala
